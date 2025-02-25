26ff3017f1fb32422976a76181d7b2be82e6539e
Win Nguyen <winnguyen1905.work@gmail.com>
2025-02-25 23:25:21 +0700

    refactor some dto file

---
+++ src/main/java/com/winnguyen1905/talk/auth/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.winnguyen1905.talk.auth;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Builder;
+
+@Builder
+public record CustomUserDetails(
+    UUID id,
+    String type,
+    String email,
+    String phone,
+    AuthRole role,
+    Boolean status,
+    String lastName,
+    String username,
+    String password,
+    String firstName) implements UserDetails {
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {  
+    return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + this.role.getName()));
+  }
+
+  @Override
+  public String getPassword() {
+    return this.password;
+  }
+
+  @Override
+  public String getUsername() {
+    return this.username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return this.status;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
---
+++ src/main/java/com/winnguyen1905/talk/common/constant/MessageStatus.java
@@ -0,0 +1,5 @@
+package com.winnguyen1905.talk.common.constant;
+
+public enum MessageStatus {
+  SENT, DELIVERED, READ
+}
